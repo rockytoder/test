381e1be6890c0238feeb8047a0b82b1394846006
clei <clei@s12.etu.info-ufr.univ-montp2.fr>
2013-09-16 14:00:41 +0200

    modified the code java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class helloworld{
-  public static void main(String[] args){
-    system.out.println("hello world");
-  }
+    public static void main(String[] args){
+        System.out.println("hello world");
+	System.out.println("holly shit");	
+    }
 }
